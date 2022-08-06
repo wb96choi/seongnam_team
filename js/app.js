36b1f0750fd060f9370efec29e35d7b9abe3b3ae
1_b1n <choi44601280@gmail.com>
2022-08-06 11:44:33 +0900

    111

--- js/app.js
+++ js/app.js
@@ -7,6 +7,17 @@ $(function(){
         });
 });
 
+// 스크롤 업다운감지해서 헤더 숨기기
+$(window).bind('mousewheel', function (event) {
+    if (event.originalEvent.wheelDelta >= 0) {
+        // console.log('Scroll up');
+        $('#header').css('transition-duration', '0.5s').css('transform', 'translateY(0)');
+    } else {
+        // console.log('Scroll down');
+        $('#header').css('transition-duration', '0.5s').css('transform', 'translateY(-100%)');
+    }
+});
+
 // 토글메뉴버튼 애니메이션
 $(function(){
     $(".hamburger").click(function(){

559d346183fae3d54ea8980155530fca0535c05d
1_b1n <choi44601280@gmail.com>
2022-08-10 12:16:33 +0900

    로그인이식성공

--- js/app.js
+++ js/app.js
@@ -1,52 +1,55 @@
 // 헤더 메뉴
-$(function(){
-        $('#header nav .gnb > li').hover(function(){
-            $(this).find('.sub').stop().slideDown();
-        },function(){
-            $(this).find('.sub').stop().slideUp();
-        });
+$(function () {
+    $('#header nav .gnb > li').hover(function () {
+        $(this).find('.sub').stop().slideDown();
+    }, function () {
+        $(this).find('.sub').stop().slideUp();
+    });
 });
 
 
 
 // 토글메뉴버튼 애니메이션
-$(function(){
-    $(".hamburger").click(function(){
+$(function () {
+    $(".hamburger").click(function () {
         $(this).toggleClass("active");
     });
-    
-    $('#toggle-menu-btn').click(function(){
+
+    $('#toggle-menu-btn').click(function () {
         $('#toggle-menu').toggleClass('on');
     })
 });
 
 // 토글메뉴의 메뉴 슬라이드 애니메이션
-$(function(){
-    $('#toggle-gnb > li').hover(function(){
+$(function () {
+    $('#toggle-gnb > li').hover(function () {
         $(this).find('.sub').stop().slideDown();
-    },function(){
+    }, function () {
         $(this).find('.sub').stop().slideUp();
     });
 });
 
 // 모바일 버전의 토글메뉴의 메뉴 슬라이드 애니메이션
-$(function(){
-    $('#toggle-gnb > li > .gnb-plus').click(function(){
+$(function () {
+    $('#toggle-gnb > li > .gnb-plus').click(function () {
         $(this).find('.sub').stop().slideDown();
     });
 });
 
+
 // 검색바 뿅 
-$(function(){
-    $('#tool-zone .container .tool-wrap .search').click(function(){
+$(function () {
+    $('#tool-zone .container .tool-wrap .search').click(function () {
         $('#tool-zone .container .tool-wrap .search-bar').toggleClass('on')
     })
 });
 
 // 탑버튼 
-$(function(){
-    $('.top').click(function(){
-        $('html, body').animate({scrollTop:0},400);
+$(function () {
+    $('.top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 400);
         return false;
     });
 })
@@ -74,12 +77,12 @@ $(function () {
 
 
         if (scrollposition >= 2000) {
-           $('#tool-zone').addClass('on');
+            $('#tool-zone').addClass('on');
         } else {
             $('#tool-zone').removeClass('on');
         }
 
-        
+
     });
 });
 
@@ -101,21 +104,21 @@ $(function () {
 
 
 // #show swiper
-$(function(){
+$(function () {
     const swiper = new Swiper('.swiper', {
         // Optional parameters
         direction: 'horizontal',
         loop: true,
-      
+
         // If we need pagination
         pagination: {
-          el: '.swiper-pagination',
+            el: '.swiper-pagination',
         },
-      
+
         // Navigation arrows
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
     });
 });
@@ -124,29 +127,37 @@ $(function(){
 
 
 // footer 
-$(function(){
-        //instagram hover
-        $("#insta").hover(
-            function() {$(this).attr("src","images/instagram_2.png");
-            },
-            function() {$(this).attr("src","images/instagram.png"); 
+$(function () {
+    //instagram hover
+    $("#insta").hover(
+        function () {
+            $(this).attr("src", "images/instagram_2.png");
+        },
+        function () {
+            $(this).attr("src", "images/instagram.png");
         });
-        //youtube hover
-        $("#youtube").hover(
-            function() {$(this).attr("src","images/youtube_2.png");
-            },
-            function() {$(this).attr("src","images/youtube.png"); 
+    //youtube hover
+    $("#youtube").hover(
+        function () {
+            $(this).attr("src", "images/youtube_2.png");
+        },
+        function () {
+            $(this).attr("src", "images/youtube.png");
         });
-        //kakao hover
-        $("#kakaotalk").hover(
-            function() {$(this).attr("src","images/kakao_2.png");
-            },
-            function() {$(this).attr("src","images/kakao.png"); 
+    //kakao hover
+    $("#kakaotalk").hover(
+        function () {
+            $(this).attr("src", "images/kakao_2.png");
+        },
+        function () {
+            $(this).attr("src", "images/kakao.png");
         });
-        //naver hover
-        $("#naver").hover(
-            function() {$(this).attr("src","images/navrer_1.png");
-            },
-            function() {$(this).attr("src","images/navrer.png"); 
+    //naver hover
+    $("#naver").hover(
+        function () {
+            $(this).attr("src", "images/navrer_1.png");
+        },
+        function () {
+            $(this).attr("src", "images/navrer.png");
         });
 });
